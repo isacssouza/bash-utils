9586b8559de22f45acce837c22be338583ce0ed9
Isac Souza <isouza@daitangroup.com>
2017-05-08 16:43:05 -0300

    Add more fields to output of findaws

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 function findaws() {
     pattern=$1
 
-    command="aws ec2 describe-instances --filters 'Name=tag:Name,Values=*${pattern}*' | jq '.Reservations[].Instances[] | (.Tags[] | select(.Key == \"Name\") | {name: .Value}) + {dns: .PublicDnsName, state: .State.Name}'"
+    command="aws ec2 describe-instances --filters 'Name=tag:Name,Values=*${pattern}*' | jq '.Reservations[].Instances[] | (.Tags[] | select(.Key == \"Name\") | {name: .Value}) + {id: .InstanceId, dns: .PublicDnsName, publicIp: .PublicIpAddress, privateIp: .PrivateIpAddress, state: .State.Name}'"
     echo ${command}
 
     eval ${command}
